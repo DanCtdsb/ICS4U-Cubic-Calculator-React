e20ef48f02b8cb42566b5b67a38a8fdf37d3585b
D:Chen <343025052@tdsb.ca>
2026-03-24 22:43:32 -0400

    Fixed a few minor issues related to default null

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { CubicEquation} from "./components/CubicEquation";
+import { CubicEquation } from "./components/CubicEquation";
 import { CubicGraph } from "./components/CubicGraph";
 import { CubicHistory } from "./components/CubicHistory";
 import { CubicInput } from "./components/CubicInput";
@@ -7,20 +7,18 @@ import type { CoefficientsType, CubicResults } from "./components/CubicInput";
 import { useState } from "react";
 
 export const App = () => {
-
   const [results, setResults] = useState<CubicResults | null>(null);
-  const [coefficients, setCoefficients] = useState<CoefficientsType | null>(null);
-
+  const [coefficients, setCoefficients] = useState<CoefficientsType | null>(
+    null,
+  );
 
   return (
     <div>
-      <CubicInput 
-      setResults={setResults}
-      setCoefficients={setCoefficients}/>
-      <CubicEquation coefficients={coefficients}/>
+      <CubicInput setResults={setResults} setCoefficients={setCoefficients} />
+      {coefficients && <CubicEquation coefficients={coefficients} />}
       <CubicGraph />
-      <CubicTable results={results}/>
+      { results && <CubicTable results={results} /> }
       <CubicHistory />
     </div>
   );
-}
+};
--- src/components/CubicTable.tsx
+++ src/components/CubicTable.tsx
@@ -1,7 +1,7 @@
 import type { CubicResults } from "./CubicInput";
 
 type CubicTableProps = {
-  results: CubicResults | null;
+  results: CubicResults;
 };
 
 export const CubicTable = ({ results }: CubicTableProps) => {
@@ -9,15 +9,15 @@ export const CubicTable = ({ results }: CubicTableProps) => {
     <table>
       <tr>
         <th>p</th>
-        <td>{results?.p.toFixed(5)}</td>
+        <td>{results.p.toFixed(5)}</td>
       </tr>
       <tr>
         <th>q</th>
-        <td>{results?.q.toFixed(5)}</td>
+        <td>{results.q.toFixed(5)}</td>
       </tr>
       <tr>
         <th>Discriminant</th>
-        <td>{results?.delta.toFixed(5)}</td>
+        <td>{results.delta.toFixed(5)}</td>
       </tr>
       <tr>
         <th>Value</th>
@@ -28,7 +28,7 @@ export const CubicTable = ({ results }: CubicTableProps) => {
       <tr>
         <th>Root 1</th>
         <td>
-          {typeof results?.root1 === "number"
+          {typeof results.root1 === "number"
             ? results.root1.toFixed(5)
             : "Complex Root"}
         </td>
@@ -37,20 +37,20 @@ export const CubicTable = ({ results }: CubicTableProps) => {
       <tr>
         <th>Root 2</th>
         <td>
-          {typeof results?.root2 === "number"
+          {typeof results.root2 === "number"
             ? results.root2.toFixed(5)
             : "Complex Root"}
         </td>
-        <td>{typeof results?.root2 === "number" ? 0 : "Complex Root"}</td>
+        <td>{typeof results.root2 === "number" ? 0 : "Complex Root"}</td>
       </tr>
       <tr>
         <th>Root 3</th>
         <td>
-          {typeof results?.root3 === "number"
+          {typeof results.root3 === "number"
             ? results.root3.toFixed(5)
             : "Complex Root"}
         </td>
-        <td>{typeof results?.root3 === "number" ? 0 : "Complex Root"}</td>
+        <td>{typeof results.root3 === "number" ? 0 : "Complex Root"}</td>
       </tr>
     </table>
   );

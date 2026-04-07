c6f114b452922efc0dfb56c507d2b9626cdad347
D:Chen <343025052@tdsb.ca>
2026-04-06 23:47:24 -0400

    Final

--- src/components/CubicEquation.tsx
+++ src/components/CubicEquation.tsx
@@ -9,19 +9,21 @@ export const CubicEquation = ({ coefficients }: CubicEquationProps) => {
   const { av, bv, cv, dv } = coefficients;
 
   t.push(av === 1 ? "x³" : `${av}x³`);
-  if (bv !== 0)
+  if (bv !== 0) {
     t.push(
       `${bv > 0 ? "+" : "-"} ${Math.abs(bv) === 1 ? "x²" : Math.abs(bv) + "x²"}`,
     );
+  }
 
-  if (cv !== 0)
+  if (cv !== 0) {
     t.push(
       `${cv > 0 ? "+" : "-"} ${Math.abs(cv) === 1 ? "x" : Math.abs(cv) + "x"}`,
     );
-    
-  if (dv !== 0)
-    t.push(`${dv > 0 ? "+" : "-"} ${Math.abs(dv)}`);
+  }
 
+  if (dv !== 0) {
+    t.push(`${dv > 0 ? "+" : "-"} ${Math.abs(dv)}`);
+  }
   return (
     <div className="text-center">
       <h1 className="text-2xl font-semibold text-gray-800">
